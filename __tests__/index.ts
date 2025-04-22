5fb0f6cf82174b7519e8edcf70545d8c1e90e522
Muhammad Rendra <mnrendra@users.noreply.github.com>
2025-04-22 16:04:01 +0700

    feat: initial feature

--- __tests__/index.ts
+++ __tests__/index.ts
@@ -1,11 +1,89 @@
-import main from '..'
+import mockedStackTrace from '@tests/mocks/stackTrace'
+import unmockStackTrace from '@tests/unmocks/stackTrace'
+
+import { getCallerFile } from '..'
+
+jest.mock('@mnrendra/stack-trace', () => ({
+  stackTrace: jest.fn()
+}))
 
 describe('Test all APIs:', () => {
-  describe('Test main API:', () => {
-    it('Should return "Hello, World!"!', () => {
-      const received = main()
-      const expected = 'Hello, World!'
-      expect(received).toBe(expected)
+  describe('Test `getCallerFile` API:', () => {
+    describe('By mocking `stackTrace` to return an invalid call site, where `getFileName` returns `undefined`:', () => {
+      beforeEach(() => {
+        mockedStackTrace.mockReturnValue([{
+          getFileName: () => undefined
+        } as unknown as NodeJS.CallSite])
+      })
+
+      afterEach(() => {
+        unmockStackTrace(mockedStackTrace)
+      })
+
+      it('Should throw an error when the returned value of `getFileName` is not a string!', () => {
+        const received = (): string => getCallerFile()
+        const expected = Error('File name is not a string: `undefined`')
+        expect(received).toThrow(expected)
+      })
+    })
+
+    describe('By mocking `stackTrace` to return an invalid call site, where `getFileName` returns `null`:', () => {
+      beforeEach(() => {
+        mockedStackTrace.mockReturnValue([{
+          getFileName: () => null
+        } as unknown as NodeJS.CallSite])
+      })
+
+      afterEach(() => {
+        unmockStackTrace(mockedStackTrace)
+      })
+
+      it('Should throw an error when the returned value of `getFileName` is not a string!', () => {
+        const received = (): string => getCallerFile()
+        const expected = Error('File name is not a string: `null`')
+        expect(received).toThrow(expected)
+      })
+    })
+
+    describe('By mocking `stackTrace` to return an invalid call site, where `getFileName` returns a non-absolute path:', () => {
+      beforeEach(() => {
+        mockedStackTrace.mockReturnValue([{
+          getFileName: () => './index.js'
+        } as unknown as NodeJS.CallSite])
+      })
+
+      afterEach(() => {
+        unmockStackTrace(mockedStackTrace)
+      })
+
+      it('Should throw an error when the returned value of `getFileName` is a non-absolute path!', () => {
+        const received = (): string => getCallerFile()
+        const expected = Error('File path is not an absolute path: "./index.js"')
+        expect(received).toThrow(expected)
+      })
+    })
+
+    describe('Without mocking anything:', () => {
+      it('Should return `__filename` when given no argument!', () => {
+        const caller = (): string => getCallerFile()
+        const received = caller()
+        const expected = __filename
+        expect(received).toBe(expected)
+      })
+
+      it('Should return `__filename` when given `null` as the argument!', () => {
+        const caller = (): string => getCallerFile(null)
+        const received = caller()
+        const expected = __filename
+        expect(received).toBe(expected)
+      })
+
+      it('Should return the caller file path when given a callee as the argument!', () => {
+        const caller = (): string => getCallerFile(caller)
+        const received = caller()
+        const expected = __filename
+        expect(received).toBe(expected)
+      })
     })
   })
 })
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,39 @@
-const main = (): string => {
-  return 'Hello, World!'
+import { isAbsolute } from 'node:path'
+
+import { stackTrace } from '@mnrendra/stack-trace'
+
+import { getFilePath } from './utils'
+
+/**
+ * Gets the caller's file path from a specific callee.
+ *
+ * Extracts the file name as an absolute path from the first call site in the
+ * stack trace. If a callee is provided, the stack trace will start from the
+ * caller of the callee.
+ *
+ * @param {((...args: any) => any) | null} [callee] - Optional callee function
+ * or method to start tracing from. If `undefined` or `null`, tracing starts
+ * from the current caller.
+ *
+ * @returns {string} A string representing the absolute path of the caller's
+ * file.
+ *
+ * @see https://github.com/mnrendra/get-caller-file#readme
+ */
+const main = (
+  callee?: ((...args: any) => any) | null
+): string => {
+  const [callSite] = stackTrace(callee ?? main, { limit: 1 })
+
+  const fileName = callSite.getFileName()
+
+  const filePath = getFilePath(fileName)
+
+  if (!isAbsolute(filePath)) {
+    throw new Error(`File path is not an absolute path: "${filePath}"`)
+  }
+
+  return filePath
 }
 
 export default main
